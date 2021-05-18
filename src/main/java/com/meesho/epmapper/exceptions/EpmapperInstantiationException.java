da315ec29723297a7ea19624c0b8cb487c71bdeb
ashishk0957 <ashish.k@meesho.com>
2021-05-18 09:00:35 +0530

    First commit

--- src/main/java/com/meesho/epmapper/exceptions/EpmapperInstantiationException.java
+++
@@ -1,7 +0,0 @@
-package com.meesho.epmapper.exceptions;
-
-public final class EpmapperInstantiationException extends EpmapperException {
-    public EpmapperInstantiationException(String message, Throwable cause){
-        super(message, cause);
-    }
-}
--- src/main/java/com/meesho/epmapper/exceptions/IllegalCastException.java
+++
@@ -1,7 +0,0 @@
-package com.meesho.epmapper.exceptions;
-
-public final class IllegalCastException extends EpmapperException{
-    public IllegalCastException(String message){
-        super(message);
-    }
-}
