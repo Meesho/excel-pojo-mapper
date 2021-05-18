7f6b3d4dd9e1e544c176c0506240537df4091bdd
ashishk0957 <ashish.k@meesho.com>
2021-05-18 10:40:45 +0530

    update

--- src/main/java/com/meesho/epmapper/dataModels/RowData.java
+++ src/main/java/com/meesho/epmapper/dataModels/RowData.java
@@ -1,5 +1,15 @@
 package com.meesho.epmapper.dataModels;
 
-public class RowData {
+import java.util.HashMap;
+import java.util.List;
 
+public class RowData {
+    public List<HashMap<String, Object>> rowData;
+
+    @Override
+    public String toString() {
+        return "RowData{" +
+                "rowData=" + rowData +
+                '}';
+    }
 }
